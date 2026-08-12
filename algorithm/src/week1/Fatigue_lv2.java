bd58d8b93ddb09c898ac43fbbdc7b76d5e51dd48
이희원 <snowman4453@gmail.com>
2026-08-12 17:45:27 +0900

    commit

--- algorithm/src/week1/Fatigue_lv2.java
+++ algorithm/src/week1/Fatigue_lv2.java
@@ -3,8 +3,10 @@ package week1;
 public class Fatigue_lv2 {
 	// 피로도
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
+	public int solution(int k, int[][] dungeons) {
+        int answer = -1;
+        return answer;
+    }
 }
