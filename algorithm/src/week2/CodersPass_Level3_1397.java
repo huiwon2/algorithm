657cdae0ab022631265a41af69091d5f40229e94
huiwon2 <snowman4453@gmail.com>
2026-08-19 21:35:14 +0900

    coderspass basic commit

--- algorithm/src/week2/CodersPass_Level3_1397.java
+++ algorithm/src/week2/CodersPass_Level3_1397.java
@@ -1,10 +1,36 @@
 package week2;
 
+import java.util.Scanner;
+
 public class CodersPass_Level3_1397 {
-
+	// 코더스패스 양방향 인접 행렬 출력
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		// 간선 
+		int A = sc.nextInt();
+		// 정점의 수 
+		int B = sc.nextInt();
+		// 출력할 인접행렬
+		int[][] graph = new int[B + 1][B + 1];
+		
+		// 간선 입력 받기 + 인접행렬 표시 
+		for(int i = 0; i < A; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			// 인접행렬(무방향 그래프) 양방향으로 1 처리 
+			graph[a][b] = 1;
+			graph[b][a] = 1;
+		}
+		
+		// 행렬 출력
+		for(int i = 1; i < graph.length; i++) {
+			for(int j = 1; j < graph[i].length; j++) {
+				System.out.print(graph[i][j]);
+			}
+			System.out.println();
+		}
+		sc.close();
 	}
 
 }
--- algorithm/src/week2/CodersPass_Level4_1145.java
+++ algorithm/src/week2/CodersPass_Level4_1145.java
@@ -1,10 +1,69 @@
 package week2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class CodersPass_Level4_1145 {
-
+	// 너비 우선 탐색
+	/** 
+	 * 전역 변수 선언
+	 * BFS 메서드에서 사용할 변수를 static으로 정의, 사용 
+	 **/
+	// 간선 개수 
+	static int N = 0;
+	// 정점 개수 
+    static int M = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static boolean[] checked;
+    private static void BFS(int v) {
+    	Queue<Integer> queue = new LinkedList<>();
+    	checked[v] = true;
+    	queue.offer(v);
+    	System.out.print(v + " ");
+    	while(!queue.isEmpty()) {
+    		int current = queue.poll();
+    		for(int next : graph.get(current)) {
+    			if(!checked[next]) {
+    				checked[next] = true;
+    				queue.offer(next);
+    				System.out.print(next + " ");
+    			}
+    		}
+    	}
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		// 시작 정점 
+		int v = sc.nextInt();
+		
+		graph = new ArrayList<ArrayList<Integer>>();
+		for(int i = 0; i < N+1; i++) {
+			graph.add(new ArrayList<Integer>());
+		}
+		
+		checked = new boolean[N+1];
+		// 간선 입력
+		for (int i = 0; i < M; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            // 그래프에 1(visited 표시)
+            if(a <= N && b <= N){
+                graph.get(a).add(b);
+                graph.get(b).add(a);
+            }
+        }
+		
+		// 더 작은 정점부터 접근하려고 할 때, graph의 오름차순 정렬이 필요
+		for(int i = 1; i < N+1; i++) {
+			Collections.sort(graph.get(i));
+		}
+		BFS(v);
+		sc.close();
 	}
 
 }
