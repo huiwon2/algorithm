cf4373600d3bbeaee3780797d2b7a34e19147174
huiwon2 <snowman4453@gmail.com>
2026-08-27 00:57:29 +0900

    [add] d5_1247 unsolved

--- algorithm/src/week2/d5_1247.java
+++ algorithm/src/week2/d5_1247.java
@@ -1,9 +1,66 @@
 package week2;
 
+import java.util.Scanner;
+
 public class d5_1247 {
 	// 최적 경로(공통)
+	static int N;
+	static int[][] points;
+	static boolean[] checked;
+	static int minDistance;
+	private static void DFS(int current, int count, int distanceSum) {
+		// Pruning
+		if(distanceSum >= minDistance) {
+			return;
+		}
+		if(count == N) {
+			int finalDistance = distanceSum + getDistance(current, N+1);
+			minDistance = Math.min(minDistance, finalDistance);
+			return;
+		}
+		// 순회
+		for(int i = 2; i < N + 2; i++) {
+			if(!checked[i]) {
+				checked[i] = true;
+				int next = getDistance(current, i);
+				DFS(i, count+1, distanceSum + next);
+				// backtracking
+				checked[i] = false;
+			}
+		}
+	}
+	// 두 좌표 사이의 거리
+	static int getDistance(int point1, int point2) {
+		// (x1 - x2) + (y1 - y2)
+		return Math.abs(points[point1][0] - points[point2][0]) + Math.abs(points[point1][1] - points[point2][1]);
+	}
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++) {
+			N = sc.nextInt();
+			points = new int[N + 2][2];
+			checked = new boolean[N + 2];
+			//회사 좌표
+			points[0][0] = sc.nextInt();
+			points[0][1] = sc.nextInt();
+			
+			// 집 좌표
+			points[1][0] = sc.nextInt();
+			points[1][1] = sc.nextInt();
+			
+			for(int i = 2; i < N+2; i++) {
+				points[i][0] = sc.nextInt();
+				points[i][1] = sc.nextInt();
+			}
+			
+			minDistance = Integer.MAX_VALUE;
+			// DFS 호출 
+			DFS(0, 0, 0);
+			System.out.println("#" + test_case + " " + minDistance);
+		}
+		sc.close();
 	}
 
 }
