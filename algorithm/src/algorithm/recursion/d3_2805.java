626d2f5237097d40b10fe38e0f631170434591a0
이희원 <snowman4453@gmail.com>
2026-09-07 17:33:50 +0900

    init

--- algorithm/src/algorithm/recursion/d3_2805.java
+++ algorithm/src/algorithm/recursion/d3_2805.java
@@ -1,9 +1,31 @@
 package algorithm.recursion;
 
+import java.util.Scanner;
+
 public class d3_2805 {
 	// 농작물 수확하기
 	public static void main(String[] args) throws Exception{
-
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++) {
+			// 농장의 크기 N
+			int N = sc.nextInt();
+			
+			// 농장의 배열
+			int garden[][] = new int[N][N];
+			
+			// 배열 농작물 입력
+			for(int i = 0; i < garden.length; i++) {
+				for(int j = 0; j < garden[i].length; j++) {
+					garden[i][j] = sc.nextInt();
+				}
+			}
+			
+			int result = 0;
+			
+			System.out.println("#" + test_case + " " + result);
+		}
+		sc.close();
 	}
 
 }
